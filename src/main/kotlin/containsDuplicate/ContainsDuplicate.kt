fe26c5371d5dd6bec7af4745284a677f83fb4731
vsened <vasilysenederov@gmail.com>
2022-10-09 02:31:56 +0500

    1

--- src/main/kotlin/containsDuplicate/ContainsDuplicate.kt
+++ src/main/kotlin/containsDuplicate/ContainsDuplicate.kt
@@ -1,5 +1,21 @@
 package containsDuplicate
 
+/**
+ * Contains Duplicate
+ *
+ * Given an integer array nums, return true if any value appears
+ * at least twice in the array, and return false if every element
+ * is distinct.
+ */
 class ContainsDuplicate {
-
+    fun containsDuplicate(nums: IntArray): Boolean {
+        val hashMap = mutableMapOf<Int, Int>()
+        for (i in nums){
+            if(hashMap.containsKey(i))
+                return true
+            else
+                hashMap[i] = 1
+        }
+        return false
+    }
 }
---
+++ src/main/kotlin/removeLinkedListElements/RemoveLinkedListElements.kt
@@ -0,0 +1,38 @@
+package removeLinkedListElements
+
+import ListNode
+
+/**
+ * Remove Linked List Elements
+ *
+ * Given the head of a linked list and an integer val, remove all
+ * the nodes of the linked list that has Node.val == val, and return
+ * the new head.
+ *
+ * Example:
+ * var li = ListNode(5)
+ * var v = li.`val`
+ * Definition for singly-linked list.
+ * class ListNode(var `val`: Int) {
+ *     var next: ListNode? = null
+ * }
+ */
+
+
+class RemoveLinkedListElements {
+    fun removeElements(head: ListNode?, `val`: Int): ListNode? {
+        if (head == null)
+            return head
+        val newHead: ListNode? = ListNode(0)
+        newHead!!.next = head
+        var currentNode: ListNode? = newHead
+        while (currentNode!!.next != null){
+            if (currentNode.next!!.`val` == `val`){
+                currentNode.next = currentNode.next!!.next
+                continue
+            }
+            currentNode = currentNode.next
+        }
+        return newHead.next
+    }
+}
